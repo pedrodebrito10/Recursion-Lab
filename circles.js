c6492b3333a9ea7ea5745445551db7db83771722
pedrodebrito10 <joao.debrito27@avenues.org>
2024-12-19 13:45:06 -0500

    Update circles.js

--- circles.js
+++ circles.js
@@ -1,11 +1,11 @@
 function drawRecursiveCircles(x, y, radius, depth) {
   if (depth <= 0) {
-    return; 
+    return; // Base case
   }
   
-
+  // to draw current circle
   ellipse(x, y, radius * 2, radius * 2); 
-
+  
+  // to draw smaller circles on both axes
   drawRecursiveCircles(x + radius / 2, y, radius / 2, depth - 1);
   drawRecursiveCircles(x, y + radius / 2, radius / 2, depth - 1);
-}
